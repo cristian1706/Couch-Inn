cff17e4e7f66b99e6c45d9869091376c4f683a40
grupo21info <cristian.csl@hotmail.com>
2016-06-03 00:52:26 -0300

    cristian

---
+++ ingenieria/EsPremium.php
@@ -0,0 +1,56 @@
+﻿<?php 
+
+$enlace = mysqli_connect("localhost", "root", "", "couchinn");
+
+/* comprobar la conexión */
+if ($enlace->connect_errno) {
+    printf("Falló la conexión: %s\n", $mysqli->connect_error);
+    exit();
+}
+session_start();
+if(isset($_SESSION['nombre']) and $_SESSION['estado'] == 'Autenticado')
+{
+       
+
+ 	 $tarjeta = $_POST['tarjeta'];        
+  	$permitidos_tarjeta = "0123456789"; 
+ 	 for ($i=0; $i<strlen($tarjeta); $i++){ 
+  	  	if (strpos($permitidos_tarjeta, substr($tarjeta,$i,1))===false){ 
+       		 echo'<script type="text/javascript">
+                alert("¡tarjeta invalida! Por favor ingrese un tarjeta solo con numeros");
+                window.location="premium.php"
+                </script>';
+   	 	}
+ 	 }
+   $fecha = $_POST['fecha'];
+   $x = "INSERT INTO premium (monto,fecha_inscripcion) VALUES ('40','$fecha')"; //agrego el premium a la bd
+   $sql=mysqli_query($enlace,$x) or die ("Error: ".mysqli_error($enlace));
+   
+
+   $consulta = "SELECT nro_inscripcion FROM premium WHERE '$fecha' = fecha_inscripcion"; //busco al num de inscripcion
+   $con=mysqli_query($enlace,$consulta) or die ("Error: ".mysqli_error($enlace));
+   $res = mysqli_fetch_array($con);
+   //var_dump($res);
+   
+   $num = $res['nro_inscripcion']; // guardo nro inscrip en num
+   $correo = $_SESSION['correo']; //guardo el correo en $correo
+   
+   $act = "UPDATE usuario SET id_nro_inscripcion = '$num' WHERE correo = '$correo'"; //actualizo al usuario agregandole el num de inscripcion
+   //var_dump($act);
+   $val=mysqli_query($enlace,$act);
+   //var_dump($val);
+   echo'<script type="text/javascript">
+   alert("¡Te has registrado como usuario premium!");
+   window.location="perfil.php"
+ </script>';
+
+
+}
+else{
+  echo'<script type="text/javascript">
+  alert("Para registrarte como premium debes iniciar sesion");
+  window.location="sesion.html"
+</script>';
+}
+
+?>
---
+++ ingenieria/agregar_tipo.php
@@ -0,0 +1,60 @@
+﻿<?php
+$enlace = mysqli_connect("localhost", "root", "", "couchinn");
+
+/* comprobar la conexión */
+if ($enlace->connect_errno) {
+    printf("Falló la conexión: %s\n", $mysqli->connect_error);
+    exit();
+}
+session_start();
+if(isset($_SESSION['correo']) and $_SESSION['estado'] == 'Autenticado'){
+	echo ("Bienvenido ".$_SESSION['correo']);}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,300italic,900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+	<title>Couch Inn</title>
+</head>
+<body>
+	<div class="cerrar">
+		<a href="modificarPerfiladmin.php"> Modificar perfil</a><br>
+		<a href="cerrarSesion.php">Cerrar Sesion</a>
+	</div>
+<header>
+	<hgroup>
+	<div class="col-md-6 center-block quitar-float text-center">
+		<img src="couchinn.png" width="500" height="100">
+	</hgroup>
+	
+		<nav class="col-md-6 center-block quitar-float text-center">
+			<ul>
+				<li><a href="indexAdmin.php">Inicio</a></li>
+				<li><a href="perfilAdmin.php">Perfil</a></li>
+				<li><a href="tipos_de_couch.php">Ver tipos de couch</a></li>
+				<li><a href="eliminar_couch.php">Eliminar tipo de couch</a></li>
+				<li><a href="faq.html">FAQ</a></li>
+
+			</ul>
+		</nav>
+		<div id="textoPr">
+			
+			<h2>Ingrrese un nuevo tipo de couch</h2><br>
+			<form action="alta_tipo.php" method="POST">
+			<label for="descripcion">Nombre tipo de couch:</label>
+			<textarea name="descripcion" rows="2" cols="25" required class="textarea"></textarea><br><br>
+			<label for="estado">Estado:</label>
+			<input name="estado" placeholder="Activo o Inactivo" required class="textarea"><br>
+			<button type='submit' id='btnSubmit'>Agregar tipo</button>
+			
+		</div>
+	</div>
+</header>
+</body>
+</html>
